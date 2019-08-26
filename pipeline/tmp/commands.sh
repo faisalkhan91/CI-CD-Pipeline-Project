b9656b4417c522d21c09c03e2c64dd868a1f1ff6
faisalkhan91 <faisalkhan91@outlook.com>
2019-08-25 20:02:49 -0400

    Created scripts to automate jar file creation

--- pipeline/tmp/commands.sh
+++ pipeline/tmp/commands.sh
@@ -2,3 +2,6 @@
 
 How to run the docker conatiner using command to build a jar file.
  -> ./jenkins/build/mvn.sh mvn -B -DskipTests clean package
+
+How to export the BUILD_TAG variable or How to set the BUILD_TAG variable value.
+ -> export BUILD_TAG=1
