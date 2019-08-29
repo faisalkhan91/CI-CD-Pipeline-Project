59606e3e1fc2239f3b0ae3d3c043babb163ee1d0
faisalkhan91 <faisalkhan91@outlook.com>
2019-08-28 21:15:13 -0400

    Created deploy script for Jenkinsfile

--- pipeline/jenkins/deploy/deploy.sh
+++ pipeline/jenkins/deploy/deploy.sh
@@ -5,4 +5,6 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth produser@192.168.143.219:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish produser@192.168.143.219:/tmp/publish
+scp -i /opt/prod produser@192.168.143.219 "/tmp/publish"
 
